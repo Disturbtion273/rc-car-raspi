8b7381283c4c541506020ca010c0c2d17add5268
Nico Breitinger <nico.breitinger@gmx.de>
2025-09-04 10:48:48 +0200

    Websocket connection messanges added

--- rc-car-raspi/Websocket.py
+++ rc-car-raspi/Websocket.py
@@ -1,6 +1,7 @@
 import threading
 from queue import Queue
 from websockets.sync.server import serve
+from websockets.exceptions import ConnectionClosed, ConnectionClosedError, ConnectionClosedOK
 
 class WebsocketServer:
     def __init__(self, WebsocketCommandHandler):
@@ -18,25 +19,38 @@ class WebsocketServer:
             server.serve_forever()
 
     def MessageHandler(self, websocket):
+        client_info = f"{websocket.remote_address[0]}:{websocket.remote_address[1]}"
+        print(f"New client connected from {client_info}")
+
         def Send():
             while True:
                 try:
                     queueItem = self.queue.get()
                     websocket.send(queueItem)
+                except ConnectionClosedError as e:
+                    print(f"Connection from {client_info} was closed abnormally: {e}")
+                    break
+                except ConnectionClosedOK as e:
+                    print(f"Connection from {client_info} was closed normally: {e}")
+                    break
                 except Exception as e:
-                    print(f"Send-Error: {e}")
-                    break  
+                    print(f"Send-Error for client {client_info}: {e}")
+                    break
 
         threading.Thread(target=Send, daemon=True).start()
 
         try:
             for message in websocket:
-                #print(f"Received message: {message}")
                 self.commandHandler.HandleMessage(message)
+        except ConnectionClosedError as e:
+            print(f"Connection from {client_info} was closed abnormally: {e}")
+        except ConnectionClosedOK as e:
+            print(f"Connection from {client_info} was closed normally: {e}")
         except Exception as e:
-            print(f"Receive-Error: {e}")
+            print(f"Receive-Error from client {client_info}: {e}")
+        finally:
+            print(f"Client {client_info} disconnected")
 
     def Send(self, message):
         self.queue.put(message)
- 
- 
+
